897d0fed1291ac195c9d80e205c56349a5689037
roblamda <roblamda@github.com>
2021-10-17 11:53:25 +0200

    upload

---
+++ cepheus/firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/cepheus/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/cepheus/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi-firmware/cepheus/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi-firmware/cepheus/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi-firmware/cepheus/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi-firmware/cepheus/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi-firmware/cepheus/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi-firmware/cepheus/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi-firmware/cepheus/imagefv.elf:install/firmware-update/imagefv.elf \
+    vendor/xiaomi-firmware/cepheus/km4.mbn:install/firmware-update/km4.mbn \
+    vendor/xiaomi-firmware/cepheus/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi-firmware/cepheus/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi-firmware/cepheus/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi-firmware/cepheus/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi-firmware/cepheus/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi-firmware/cepheus/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+    vendor/xiaomi-firmware/cepheus/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi-firmware/cepheus/xbl_config.elf:install/firmware-update/xbl_config.elf
